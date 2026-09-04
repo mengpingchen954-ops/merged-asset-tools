60bc7436b9a158f6bead311ebc2217abfe64fa42
unknown <chenmengping@q1oa.com>
2026-09-04 15:25:36 +0800

    Connect CUTFRAME credits to Supabase

--- cutframe/supabase-config.js
+++ cutframe/supabase-config.js
@@ -1,4 +1,4 @@
 window.CUTFRAME_SUPABASE = Object.freeze({
-  url: "",
-  anonKey: "",
+  url: "https://olkzjegcmuwxuvuedmfh.supabase.co",
+  anonKey: "sb_publishable_-J_Z_UdoJ45haTdZMegZlw_XsC59pbt",
 });
